19b892f6b00affcb205f7c817dbca8a7ba41b5b6
Viktor Belov <vbelov@vansofts.net>
2023-08-29 16:29:30 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+   n = `${n}`;
+    let i = 0;
+    let result = '';
+    while (i < n.length) {
+    result = `${n[i]}${result}`;
+    i = i + 1;
+   }
+   if (result.slice(-1) == '-') {
+    return result.slice(0,(n.length - 1));
+   }    else return result;
 }
